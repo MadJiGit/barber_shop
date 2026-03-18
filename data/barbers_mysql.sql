b3eb5fe1f35744af14e851693337255161dee6e3
Mladen Raykov <raykovmladen@gmail.com>
2026-03-18 15:57:12 +0200

    add .htaccess file

--- data/barbers_mysql.sql
+++ data/barbers_mysql.sql
@@ -7,4 +7,6 @@ VALUES
 ('admin@abv.bg', '["ROLE_ADMIN"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Admin', 'Adminev', 'Admina', '0888888811', NOW(), 1, 0),
 ('barber_senior@abv.bg', '["ROLE_BARBER_SENIOR"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Senior', 'Barber', 'Seniora', '0888888888', NOW(), 1, 0),
 ('barber@abv.bg', '["ROLE_BARBER"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Barber', 'Barber', 'Barbara', '0999999999', NOW(), 1, 0),
-('client_1@abv.bg', '["ROLE_CLIENT"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Client', 'First', 'Clienta', '0999999966', NOW(), 1, 0);
+('client_1@abv.bg', '["ROLE_CLIENT"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Client', 'First', 'Clienta', '0999999966', NOW(), 1, 0),
+('client_2@abv.bg', '["ROLE_CLIENT"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Client', 'Second', 'Clienta 2', '0999999967', NOW(), 1, 0),
+('client_3@abv.bg', '["ROLE_CLIENT"]', '$2y$13$LVQe5U5gu3IRJoNZps/OruwBC3EieX6o.Mo4Nba0LGkxACe9kipzS', 'Client', 'Third', 'Clienta 3', '0999999968', NOW(), 1, 0);
